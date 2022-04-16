da777ca0713d747fcafebff4dcf4183c0c20daa2
Cora Iberkleid <ciberkleid@gmail.com>
2022-04-15 22:25:01 -0400

    install app toolkit

---
+++ install.sh
@@ -0,0 +1,9 @@
+if [[ -z ${KP_REPO} || -z ${KP_USERNAME} || -z ${KP_PASSWORD} ]]; then
+  echo "The following environment variables must be set:"
+  echo "     KP_REPO, KP_USERNAME, KP_PASSWORD"
+  exit 1
+fi
+
+envsubst < install-values-REDACTED.yaml > install-values.yaml
+
+tanzu package install app-toolkit --package-name app-toolkit.community.tanzu.vmware.com --version 0.1.0 -f install-values.yaml -n tanzu-package-repo-global
