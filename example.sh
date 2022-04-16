6c713ab7eed9c7d58782106263e98e6ef807d2e9
Cora Iberkleid <ciberkleid@gmail.com>
2022-04-15 20:06:29 -0400

    still not working

--- example.sh
+++ example.sh
@@ -1,16 +1,28 @@
-# Configure cluster tooling (rbac and kpack)
+##### CLUSTER OPERATOR PERSPECTIVE
+if [[ -z ${REGISTRY_URL} || -z ${REGISTRY_USERNAME} || -z ${REGISTRY_PASSWORD} || ${IMAGE_PREFIX} ]]; then
+  echo "The following environment variables must be set:"
+  echo "     REGISTRY_URL, REGISTRY_USERNAME, REGISTRY_PASSWORD, IMAGE_PREFIX"
+  exit 1
+fi
 
-# Apply Acccess Control
-kapp deploy --yes -a example-rbac -f <(ytt --ignore-unknown-comments -f example/cluster/ -f example-values.yaml)
+##### CLUSTER OPERATOR PERSPECTIVE
+# Log in to docker locally
+echo $REGISTRY_PASSWORD | docker login -u ${REGISTRY_USERNAME} --password-stdin ${REGISTRY_URL}
 
-# Create a stack, store and builder
+# kpack configuration: create a stack, store and builder for kpack
 kp clusterstack save base --build-image paketobuildpacks/build:base-cnb --run-image paketobuildpacks/run:base-cnb
 kp clusterstore save default -b gcr.io/paketo-buildpacks/java -b gcr.io/paketo-buildpacks/go
-kp clusterbuilder save builder --tag harbor.tanzu.coraiberkleid.site/demo/builder --stack base --store default --order cfg/kpack-builder-order.yaml
+kp clusterbuilder save builder --tag ${IMAGE_PREFIX}builder --stack base --store default --order example/cluster/kpack-builder-order.yaml
 
-# Install Cartographer templates and supply chain
+##### APP OPERATOR PERSPECTIVE
+# Cartographer configuration:
+# Apply Acccess Control
+kapp deploy --yes -a example-rbac -f <(ytt --ignore-unknown-comments -f example/cluster/ -f example-values.yaml)
+# Create templates and supply chain
 kapp deploy --yes -a example -f <(ytt --ignore-unknown-comments -f example/app-operator/ -f example-values.yaml)
 
+
+##### DEVELOPER PERSPECTIVE
 # Apply workload
 kubectl apply -f example/developer/workload.yaml
 
