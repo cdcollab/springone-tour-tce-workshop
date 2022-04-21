f230f5f3581fe0f38d4fe9d7e14504ddceab3421
DaShaun <dashaun@dashaun.com>
2022-04-21 14:05:43 -0500

    vendir/bin

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -7,4 +7,11 @@ ytt -f vendir.yml \
   --data-values-file <(envsubst < values-prerequisites.yaml) \
   | vendir sync -f-
 
+# Soft link files for consistency into path
+mkdir vendir/bin
+mv vendir/binaries/kn/kn* vendir/bin/kn
+mv vendir/binaries/kp/kp* vendir/bin/kp
+mv vendir/binaries/kubectl/kubectl* vendir/bin/kubectl
+mv vendir/binaries/yq/yq* vendir/bin/yq
+chmod +x vendir/bin/*
 tar -xzvf vendir/binaries-tce/*.tar.gz -C vendir
